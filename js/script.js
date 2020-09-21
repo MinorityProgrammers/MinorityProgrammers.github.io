e388c64233d0d78aeae8032704c485cb29cc0b5e
Shot <shot@minorityprogrammers.org>
2020-09-21 01:19:09 -0400

    comment

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function reveal(idname) {
+    document.getElementById(idname).style.display = "flex";
+}
+
+function changeBorder(idname) {
+    document.getElementById(idname).style.borderColor = "#FFC700";
+}
+
+
+
+function joinButton() {
+    alert("Welcome to the Minority Programmers!");
+
+}
